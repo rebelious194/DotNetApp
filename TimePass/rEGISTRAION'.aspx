d890688ce00dc53fa15020c5a7b40530a6ab4605
rebelious194 <rebelious.194@gmail.com>
2017-12-12 23:10:09 +0530

    new build

---
+++ TimePass/rEGISTRAION'.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="rEGISTRAION'.aspx.cs" Inherits="TimePass.rEGISTRAION_" %>
+
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="Label1" runat="server" Text="Dname"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+     <asp:Label ID="Label2" runat="server" Text="Location"></asp:Label>
+     <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+  <asp:Button runat="server" OnClick="insert" text="submit" />    </div>
+
+        <asp:DropDownList ID="DropDownList1" runat="server"></asp:DropDownList>
+
+        <asp:gridview ID="Gridview1" runat="server"></asp:gridview>
+    </form>
+</body>
+
+</html>
---
+++ TimePass/rEGISTRAION'.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TimePass
+{
+    public partial class rEGISTRAION_ : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connection = "Data Source=DESKTOP-MSFKHGD;Initial Catalog=employee;Integrated Security=true;";
+            SqlConnection scon = new SqlConnection(connection);
+            scon.Open();
+            SqlCommand scmd = new SqlCommand("select dname from dept", scon);
+            DataTable dt = new DataTable();
+            SqlDataAdapter sda = new SqlDataAdapter(scmd);
+            sda.Fill(dt);
+
+            DropDownList1.DataSource = dt;
+            DropDownList1.DataTextField = "dname";
+            DropDownList1.DataBind();
+
+            scon.Close();
+            bindgrid();
+
+        }
+
+
+        public void bindgrid()
+        {
+            string connection = "Data Source=DESKTOP-MSFKHGD;Initial Catalog=employee;Integrated Security=true;";
+            SqlConnection scon = new SqlConnection(connection);
+            scon.Open();
+            SqlCommand scmd = new SqlCommand("select * from dept", scon);
+            DataTable dt = new DataTable();
+            SqlDataAdapter sda = new SqlDataAdapter(scmd);
+            sda.Fill(dt);
+
+            Gridview1.DataSource = dt;
+            Gridview1.DataBind();
+
+            scon.Close();
+        }
+
+        protected void insert(object sender, EventArgs e)
+        {
+            string connection = "Data Source=DESKTOP-MSFKHGD;Initial Catalog=employee;Integrated Security=true;";
+            SqlConnection scon = new SqlConnection(connection);
+            scon.Open();
+            SqlCommand scmd = new SqlCommand("insert into dept values(12,@name,@location)",scon);
+            scmd.Parameters.Add("@name", TextBox1.Text);
+            scmd.Parameters.Add("@location", TextBox2.Text);
+            scmd.ExecuteNonQuery();
+            scon.Close();
+
+        }
+
+
+
+    }
+}
